884fea46db313593a4cb59c70d01da47e48c2fe1
Quentin <qdorme@gmail.com>
2026-06-13 23:19:53 +0200

    avec ihm v_alpha

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use image::{ImageBuffer, Rgb, RgbImage, Rgba};
+use macroquad::prelude::{
+    BLACK, FilterMode, Texture2D, WHITE, clear_background, draw_texture, next_frame,
+};
 use rand::RngExt;
+use std::env;
+use std::ptr::slice_from_raw_parts;
 use tracing::info;
 
 const NORTH: u8 = 0b00000001;
@@ -7,6 +12,10 @@ const EAST: u8 = 0b00000010;
 const SOUTH: u8 = 0b00000100;
 const WEST: u8 = 0b00001000;
 const ALL_WALLS: u8 = 0b00001111;
+const VISITED: u8 = 0b00010000;
+const CURRENT: u8 = 0b00100000;
+const CLEAR: u8 = 0b11001111;
+const ENTRY: u8 = 0b01000000;
 
 struct Maze {
     width: u32,
@@ -25,44 +34,38 @@ impl Maze {
         }
     }
 
-    fn generate_maze(&mut self, start: (u32, u32)) {
-        self.paths.push(start);
-        info!("Starting maze generation at cell: {:?}", start);
-
-        while !self.paths.is_empty() {
-            if let Some(cell) = self.choose_unvisited_neighbor() {
-                if let Some(&(x, y)) = self.paths.last() {
-                    let (new_x, new_y, direction) = cell;
-                    // on enlève le mur entre les deux cellules
-                    match direction {
-                        NORTH => {
-                            self.walls[y as usize][x as usize] &= !NORTH;
-                            self.walls[new_y as usize][new_x as usize] &= !SOUTH;
-                        }
-                        SOUTH => {
-                            self.walls[y as usize][x as usize] &= !SOUTH;
-                            self.walls[new_y as usize][new_x as usize] &= !NORTH;
-                        }
-                        EAST => {
-                            self.walls[y as usize][x as usize] &= !EAST;
-                            self.walls[new_y as usize][new_x as usize] &= !WEST;
-                        }
-                        WEST => {
-                            self.walls[y as usize][x as usize] &= !WEST;
-                            self.walls[new_y as usize][new_x as usize] &= !EAST;
-                        }
-                        _ => {}
+    pub fn generate_maze_by_step(&mut self) {
+        if let Some(cell) = self.choose_isolated_neighbor() {
+            if let Some(&(x, y)) = self.paths.last() {
+                let (new_x, new_y, direction) = cell;
+                // on enlève le mur entre les deux cellules
+                match direction {
+                    NORTH => {
+                        self.walls[y as usize][x as usize] &= !NORTH;
+                        self.walls[new_y as usize][new_x as usize] &= !SOUTH;
                     }
+                    SOUTH => {
+                        self.walls[y as usize][x as usize] &= !SOUTH;
+                        self.walls[new_y as usize][new_x as usize] &= !NORTH;
+                    }
+                    EAST => {
+                        self.walls[y as usize][x as usize] &= !EAST;
+                        self.walls[new_y as usize][new_x as usize] &= !WEST;
+                    }
+                    WEST => {
+                        self.walls[y as usize][x as usize] &= !WEST;
+                        self.walls[new_y as usize][new_x as usize] &= !EAST;
+                    }
+                    _ => {}
                 }
-                self.paths.push((cell.0, cell.1));
-            } else {
-                self.paths.pop();
             }
+            self.paths.push((cell.0, cell.1));
+        } else {
+            self.paths.pop();
         }
     }
 
-    fn choose_unvisited_neighbor(&self) -> Option<(u32, u32, u8)> {
-
+    fn choose_isolated_neighbor(&self) -> Option<(u32, u32, u8)> {
         match self.paths.last() {
             Some(&(x, y)) => {
                 let mut neighbors = vec![];
@@ -96,39 +99,69 @@ impl Maze {
             None => None,
         }
     }
+
+    pub fn find_entry(&mut self, starting_cell: (u32, u32)) -> Option<(u32, u32)> {
+        self.walls[starting_cell.1 as usize][starting_cell.0 as usize] &= VISITED;
+        let mut neighbor_cells = self.choose_unvisited_neighbor(starting_cell);
+
+        None
+    }
+
+    fn choose_unvisited_neighbor(&self, current_cell: (u32, u32)) -> Vec<(u32, u32)> {
+        let mut neighbors = vec![];
+
+        let (x, y) = current_cell;
+        if y > 0 && self.walls[(y - 1) as usize][x as usize] & VISITED != VISITED {
+            neighbors.push((x, y - 1));
+        }
+        if y + 1 < self.height && self.walls[(y + 1) as usize][x as usize] & &VISITED != VISITED {
+            neighbors.push((x, y + 1));
+        }
+        if x > 0 && self.walls[y as usize][(x - 1) as usize] & &VISITED != VISITED {
+            neighbors.push((x - 1, y));
+        }
+        if x + 1 < self.width && self.walls[y as usize][(x + 1) as usize] & &VISITED != VISITED {
+            neighbors.push((x + 1, y));
+        }
+
+        neighbors
+    }
 }
 
-fn generate_image(maze: &Maze) {
+fn generate_image(maze: &Maze) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let square_size = 10;
     let mut img = RgbImage::new(
-        (maze.width+2)*square_size, (maze.height+2)*square_size);
+        (maze.width + 2) * square_size,
+        (maze.height + 2) * square_size,
+    );
     img.fill(255); // fond blanc
 
     maze.walls.iter().enumerate().for_each(|(y, row)| {
         row.iter().enumerate().for_each(|(x, cell)| {
             let corners = (
-                ((x+1) as u32 * square_size, (y+1) as u32 * square_size ),
-                ((x+2) as u32 * square_size, (y+1) as u32 * square_size),
-                ((x+2) as u32 * square_size, (y+2) as u32 * square_size),
-                ((x+1) as u32 * square_size, (y+2) as u32 * square_size ));
+                ((x + 1) as u32 * square_size, (y + 1) as u32 * square_size),
+                ((x + 2) as u32 * square_size, (y + 1) as u32 * square_size),
+                ((x + 2) as u32 * square_size, (y + 2) as u32 * square_size),
+                ((x + 1) as u32 * square_size, (y + 2) as u32 * square_size),
+            );
 
             if *cell & NORTH == NORTH {
-                (corners.0.0 .. corners.1.0).for_each(|x| {
+                (corners.0.0..corners.1.0).for_each(|x| {
                     img.put_pixel(x, corners.0.1, Rgb([0, 0, 0]));
                 })
             }
             if *cell & SOUTH == SOUTH {
-                (corners.3.0 .. corners.2.0).for_each(|x| {
+                (corners.3.0..corners.2.0).for_each(|x| {
                     img.put_pixel(x, corners.2.1, Rgb([0, 0, 0]));
                 })
             }
             if *cell & EAST == EAST {
-                (corners.1.1 .. corners.2.1).for_each(|y| {
+                (corners.1.1..corners.2.1).for_each(|y| {
                     img.put_pixel(corners.1.0, y, Rgb([0, 0, 0]));
                 })
             }
             if *cell & WEST == WEST {
-                (corners.0.1 .. corners.3.1).for_each(|y| {
+                (corners.0.1..corners.3.1).for_each(|y| {
                     img.put_pixel(corners.0.0, y, Rgb([0, 0, 0]));
                 })
             }
@@ -136,27 +169,69 @@ fn generate_image(maze: &Maze) {
     });
 
     img.save("maze.png").expect("Échec");
+    img
 }
 
-fn main() {
-
+#[macroquad::main("Maze")]
+async fn main() {
     tracing_subscriber::fmt::fmt().init();
 
+    let mut width = 30;
+    let mut height = 30;
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 3 {
+        width = args[1].parse().expect("Invalid width");
+        height = args[2].parse().expect("Invalid height");
+    }
+
+    info!("Starting maze generation ({}, {})", width, height);
+    info!("Initialization phase");
 
     let mut random = rand::rng();
 
-    let mut maze = Maze::new(30, 30);
+    let mut maze = Maze::new(width, height);
     let starting_cell = (
         random.random_range(0..maze.width),
         random.random_range(0..maze.height),
     );
 
-    maze.generate_maze(starting_cell);
+    info!("Starting at cell: {:?}", starting_cell);
+    maze.paths.push(starting_cell);
 
-    generate_image(&maze);
+    loop {
+        if !maze.paths.is_empty() {
+            info!("Generation phase");
 
-    info!(
-        "Maze created with width: {} and height: {}",
-        maze.width, maze.height
-    );
+            maze.generate_maze_by_step();
+        } else {
+            info!("Finding path phase");
+
+            maze.find_entry(starting_cell.clone());
+        }
+        info!("Image generation phase");
+
+        let img = generate_image(&maze);
+
+        let texture = convert_to_texture(img);
+
+        clear_background(BLACK);
+        draw_texture(&texture, 0.0, 0.0, WHITE);
+        next_frame().await;
+    }
+}
+
+fn convert_to_texture(img: ImageBuffer<Rgb<u8>, Vec<u8>>) -> Texture2D {
+    let (w, h) = img.dimensions();
+    let raw = img.into_raw(); // Vec<u8> RGB
+
+    // Convertir en RGBA pour macroquad
+    let rgba: Vec<u8> = raw
+        .chunks(3)
+        .flat_map(|p| [p[0], p[1], p[2], 255])
+        .collect();
+
+    let texture = Texture2D::from_rgba8(w as u16, h as u16, &rgba);
+    texture.set_filter(FilterMode::Nearest);
+    texture
 }
